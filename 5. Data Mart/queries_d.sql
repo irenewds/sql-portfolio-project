13c0bbe99849a8b767739a0232bc47ba6d107f4b
Irene Widyastuti <widyasirene@gmail.com>
2026-04-28 16:02:00 +0700

    adding project files

---
+++ 5. Data Mart/queries_d.sql
@@ -0,0 +1,9 @@
+USE data_mart;
+
+-- D. Bonus Question
+-- Which areas of the business have the highest negative impact in sales metrics performance in 2020 for the 12 week before and after period?
+-- region
+-- platform
+-- age_band
+-- demographic
+-- customer_type
